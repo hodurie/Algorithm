65ff1cb9ff12360436253766610ed4f70b52e1d1
hodurie <hodurie@gmail.com>
2020-05-27 21:02:25 +0900

    [Java] JUNGOL

---
+++ JUNGOL/src/Operator520.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Operator520 {
+	public static void main(String[] args) {
+		int a, b;
+		a = 0;
+		
+		Scanner scn = new Scanner(System.in);
+		a = scn.nextInt();
+		System.out.println(a++);
+		System.out.println(++a);
+	}
+}
