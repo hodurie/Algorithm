55cd894f8e343517e8c35d1c02322afd1705bd96
hodurie <hodurie@gmail.com>
2020-06-22 21:21:20 +0900

    [Java] SWExpertAcademy

--- SWExpertAcademy/src/D2/SW_1946.java
+++ SWExpertAcademy/src/D2/SW_1946.java
@@ -2,41 +2,36 @@ package D2;
 
 import java.util.Scanner;
 
-// 오류 나는 이유 확인하기.
 public class SW_1946 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		int n = 0;
-		int num = 0;
-		String str = "";
-		int cnt = 0;
-
+		
 		for (int t = 0; t < T; t++) {
-			n = sc.nextInt();
-
-			System.out.println("#" + (t + 1));
-
-			while (true) {
-				if (n == 0) {
-					break;
-				}
-				str = sc.next();
-				num = sc.nextInt();
-
-				for (int i = 0; i < num; i++) {
-					System.out.print(str);
-					if (cnt % 10 == 9)
-						System.out.println();
-					cnt++;
-				}
-
-				n--;
+			int size = sc.nextInt();
+			String[] letter = new String[size];
+			int[] num = new int[size];
+			
+			for (int i = 0; i < size; i++) {
+				letter[i] = sc.next();
+				num[i] = sc.nextInt();
 			}
-			System.out.print(" ");
+			
+			int count = 0;
+			System.out.println("#" + t+1);
+			
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < num[i]; j++) {
+					System.out.print(letter[i]);
+					count++;
+					
+					if (count == 10) {
+						System.out.println();
+						count = 0;
+					}
+				}
+			}
 			System.out.println();
-
 		}
 	}
-
 }
