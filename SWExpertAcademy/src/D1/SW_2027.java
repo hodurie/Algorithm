4cd6f8714ed142ef94f8754e5e7facfd78cbfffa
hodurie <hodurie@gmail.com>
2020-06-29 20:25:41 +0900

    [Java] SWExpertAcademy

---
+++ SWExpertAcademy/src/D1/SW_2027.java
@@ -0,0 +1,12 @@
+package D1;
+
+class SW_2027{
+	public static void main(String args[]) throws Exception{
+        System.out.println("#++++");
+        System.out.println("+#+++");
+        System.out.println("++#++");
+        System.out.println("+++#+");
+        System.out.println("++++#");
+
+	}
+}
