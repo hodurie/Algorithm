df54b3116aadd9a13254c2f0091303c5ab724765
hodurie <hodurie@gmail.com>
2020-07-07 23:41:20 +0900

    [Java] SWExpertAcademy

---
+++ SWExpertAcademy/src/D1/SW_2046.java
@@ -0,0 +1,14 @@
+package D1;
+
+import java.util.Scanner;
+
+public class SW_2046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int t = 0; t < T; t++){
+			System.out.print("#");
+		}
+	}
+};
