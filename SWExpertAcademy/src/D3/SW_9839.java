395335ca66f32b51453ceea001cca468ba94be0d
hodurie <hodurie@gmail.com>
2020-07-19 18:35:06 +0900

    [Java] SWExpertAcademy
    9839 문제 해결

--- SWExpertAcademy/src/D3/SW_9839.java
+++ SWExpertAcademy/src/D3/SW_9839.java
@@ -1,8 +1,8 @@
 package D3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-// 수정 필요
 public class SW_9839 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -12,40 +12,42 @@ public class SW_9839 {
 		for (int t = 0; t < T; t++) {
 			int N = sc.nextInt();
 			int[] arr = new int[N];
-			String mul = "";
-			int max = 0;
+			int[] mul = new int[N*(N-1)/2];
+			int max = -1;
 			int cnt = 0;
-			boolean check = false;
-
+			
 			for (int n = 0; n < N; n++) {
 				arr[n] = sc.nextInt();
 			}
-
+			 
 			for (int i = 0; i < N - 1; i++) {
 				for (int j = i + 1; j < N; j++) {
-					mul = Integer.toString(arr[i] * arr[j]);
-					for (int m = 0; m < mul.length(); m++) {
-						if (mul.length() == 1) {
-							check = true;
-							max = Integer.parseInt(mul);
-						} else {
-							if (Integer.parseInt(mul, m) + 1 == Integer.parseInt(mul, m + 1)) {
-								cnt++;
-							} else {
-								break;
-							}
-
-							if (m == (mul.length() - 1) && cnt == (mul.length() - 1)) {
-								check = true;
-								max = Integer.parseInt(mul);
-							}
-						}
-					}
+					mul[cnt] = arr[i] * arr[j];
+					cnt++;
+				}
+			}
+			
+			Arrays.sort(mul);
+			
+			for(int i = mul.length-1; i > -1; i--) {
+				cnt = 0;
+				String str = Integer.toString(mul[i]);
+				for(int j = 0; j < str.length() -1; j++) {
+					int a = Integer.parseInt(str.substring(j, j + 1));
+                    int b = Integer.parseInt(str.substring(j + 1, j + 2));
+                    if (a != b - 1) {
+                        break;
+                    }else {
+                    	cnt++;
+                    }
+				}
+				if(cnt == str.length() -1) {
+					max = mul[i];
+					break;
 				}
 			}
 
-			System.out.printf("#%d %d\n", (t + 1), check == true ? max : -1);
+			System.out.printf("#%d %d\n", (t + 1), max);
 		}
-
 	}
 }
