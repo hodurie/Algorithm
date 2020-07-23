71832cee5d254cf10675365eb777d64fc1f8dfc7
hodurie <hodurie@gmail.com>
2020-07-23 20:07:30 +0900

    [Java] SWExpertAcademy

--- SWExpertAcademy/src/D2/SW_2005.java
+++ SWExpertAcademy/src/D2/SW_2005.java
@@ -8,18 +8,18 @@ class SW_2005 {
         
         int T = sc.nextInt();
         
-        for(int t=1; t<=T; t++) {
+        for(int t = 1; t <= T; t++) {
             System.out.format("#%d\n", t);
             int n = sc.nextInt();
-            int cur[] = new int[n+2];
-            int next[] = new int[n+2];
+            int cur[] = new int[n + 2];
+            int next[] = new int[n + 2];
             cur[1] = 1;
             int i, j;
             for(i=1; i<=n; i++) {
-                for(j=1; j<i+1; j++) System.out.format("%d ", cur[j]);
+                for(j = 1; j < i + 1; j++) System.out.format("%d ", cur[j]);
                 System.out.println();
-                for(j=1; j<i+2; j++) next[j] = cur[j-1]+cur[j];
-                for(j=1; j<i+2; j++) cur[j] = next[j];
+                for(j = 1; j < i + 2; j++) next[j] = cur[j - 1] + cur[j];
+                for(j = 1; j < i + 2; j++) cur[j] = next[j];
             }
         }
     }
