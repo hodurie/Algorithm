7d718c5bba578c1c52f0f524734c314206099b57
hodurie <hodurie@gmail.com>
2021-04-28 23:02:56 +0900

    [Python] Baekjoon OJ

---
+++ Baekjoon/Silver/1904.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+array = [0 for i in range(1000001)]
+
+array[1] = 1
+array[2] = 2
+
+for i in range(3, n+1):
+    array[i] = (array[i-2] + array[i-1]) % 15746
+
+print(array[n])
